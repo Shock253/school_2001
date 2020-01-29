f005b34d44357dddc287ef206b6dd9edcf47ecea
Ethan Hocking <eshocking253@gmail.com>
2020-01-29 13:20:13 -0700

    Add attr_reader for hours_in_school_day, refactor name to match

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,8 @@
 class School
-  attr_reader :start_time
-  def initialize(start_time, school_day_hours)
+  attr_reader :start_time, :hours_in_school_day
+  def initialize(start_time, hours_in_school_day)
     @start_time = start_time
-    @school_day_hours = school_day_hours
+    @hours_in_school_day = hours_in_school_day
   end
+  
 end
